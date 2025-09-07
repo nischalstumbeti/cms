ef3e478f3316ae6fc3e6a0fa24b399d294af6f6e
nischalstumbeti <nischals.tumbeti@gmail.com>
2025-09-07 22:50:24 +0530

    fiixed tittle and ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'ContestZen',
+  title: 'Chitra Manthan',
   description: 'Unleash Creativity, Seamlessly.',
 };
 
